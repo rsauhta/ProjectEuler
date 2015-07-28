58684aad05730fd7479fa525c4b8cd02f28a52be
Rohit Sauhta <rsauhta@yahoo.com>
2015-07-28 11:03:29 +0530

    Added an alternative faster implementation for problem #92

--- p92.py
+++ p92.py
@@ -7,38 +7,69 @@ def getDigitSum(n):
 		digitSum += int(digit)*int(digit)
 	return digitSum
 
+def getSquareChainDigitSum(num):
+	"Converge square digit to 89 or 1, without using any cache"
 
-# largest possible digital sum for numbers upto 10 Million would be 
-#   9^2 * 7 digits = 567
-CacheNumberRange = 568
-TestNumberRange = 10000000
+	if num == 0: 
+		return 0
 
-SquareChainList = [0 for x in range(0,CacheNumberRange)]
-
-
-for i in range(1,CacheNumberRange):
-	num = i
 	while (1):
 		digitSum = getDigitSum(num)
 		if digitSum == 1: 
-			SquareChainList[i] = 1
-			break
+			return 1
 		elif digitSum == 89: 
-			SquareChainList[i] = 89
-			break
+			return 89
 		else:
 			num = digitSum
 
 
-counter = 0
-# Re-computing some numbers but those are a small fraction
-for i in range(1,TestNumberRange):
-	digitSum = getDigitSum(i)
-	if SquareChainList[digitSum] == 89:   # normally I would put an asser there but index de-reference will 
-	                                      #  throw exception if my assumption is wrong
-		counter += 1
 
 
-print counter
+
+def approach1(numberRange):
+	# largest possible digital sum for numbers upto 10 Million would be 
+	#   9^2 * 7 digits = 567
+	CacheNumberRange = 568
+
+	SquareChainList = [getSquareChainDigitSum(x) for x in range(0,CacheNumberRange)]
+
+	counter = 0
+	# Re-computing some numbers but those are a small fraction
+	for i in range(1,TestNumberRange):
+		digitSum = getDigitSum(i)
+		if SquareChainList[digitSum] == 89:   # normally I would put an assert here but index de-reference will 
+		                                      #  throw exception if my assumption is wrong
+			counter += 1
+	return counter
+
+def approach2(numberRange):
+
+	squareChainDict = {0:1}        # seed value
+	digitSquareList = [n*n for n in range(0,10)]    # we could just compute square everytime but this might be a bit faster
+
+	# look at all combination for digit in each position
+	for position in range(1,8): 	# <10 million has 7 digits
+		newDict = {}
+		for num,count in squareChainDict.iteritems(): # for each existing digit square sum, find 
+		                                              #   combinations obtained by adding square for 
+							      #   the next digit
+			for digitSquare in digitSquareList:
+				newSum = num + digitSquare
+				if newSum in newDict:
+					newDict[newSum] += count
+				else:
+					newDict[newSum] = count
+		squareChainDict = newDict
+		
+	counter=0
+	for num,count in squareChainDict.iteritems():
+		if getSquareChainDigitSum(num)  == 89:
+			counter += count
+	return counter
+		
+	
+
+TestNumberRange = 10000000
+print approach2(TestNumberRange)
 
 
