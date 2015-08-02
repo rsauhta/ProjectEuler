963c8f26b3bc64fa39d9eba8208f564fffb5846a
Rohit Sauhta <rsauhta@yahoo.com>
2015-08-02 13:54:54 +0530

    Committing solution for #118 without checking on site. Project Euler site is down due to a hacking attempt. Big integer addition appears to be fairly quick in Python but toString conversion is not that fast. Using modulo addition for trailing 9 digits sped up the program substantially.

---
+++ p104.py
@@ -0,0 +1,52 @@
+# https://projecteuler.net/problem=104
+#
+
+
+def isPandigital(panStr):
+	if (len(panStr) < 9):
+		return False
+	for digit in range(1,10):
+		if str(digit) not in panStr:
+			return False
+	return True
+
+
+
+assert(isPandigital("123456789") == True)
+assert(isPandigital("123456709") == False)
+
+	
+fibOne = 1
+fibTwo = 1 
+fibCounter=2
+
+trailerOne = 1
+trailerTwo = 1
+
+while True:
+	temp = fibTwo
+	fibTwo = fibOne + fibTwo
+	fibOne = temp
+
+	temp = trailerTwo
+	trailerTwo = (trailerOne + trailerTwo) % 10**9   # Only track last 9 digits
+	trailerOne = temp
+
+	fibCounter += 1
+
+	#if fibCounter % 100 == 0:
+		#print fibCounter
+
+	
+	if isPandigital(str(trailerTwo)):
+		print "Trailing matches ", fibCounter
+	else: 
+		continue
+
+	fibStr = str(fibTwo)
+	if isPandigital(fibStr[:9]):
+		print "Leading matches. Answer is ", fibCounter
+		break
+
+
+
