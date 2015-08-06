6f95a9b3bb7b4fc117e9b4f5d920240c0da8807b
Rohit Sauhta <rsauhta@yahoo.com>
2015-08-05 19:47:49 -0700

    Optimized implementation for #151 using memoization

--- p151.py
+++ p151.py
@@ -1,42 +1,57 @@
 # https://projecteuler.net/problem=151
 #
 
+ProbCache = {}
 
-def findProb(pageList, probList, probIn, batchNum):
+# Add an entry for terminating the last batch
+ProbCache[str([5])] = [0.0 for i in range(0,16)]
+
+def findProb(pageList, batchNum=0):
         ''' 
         Compute probability of finding 1 sheet. 
           pageList contains list of pages represented by size (a1=1, a2=2..)
-          probList[batchNum] is where we will store the probability
-          probIn is probability of this particular event 
+
+          a list of probability is returned 
+             probList[batchNum] is where we will store the probability for current batch
+                  values for all batches after this batch will also be stored in probList
+             ProbCache is updated with an entry for this combination of pages
         '''
 
-        if batchNum <= 7: 
-                print probIn, pageList
+        global probCache
 
+
+        pageList = sorted(pageList)
+        pageListStr = str(pageList)
+        if pageListStr in ProbCache:
+                return ProbCache[pageListStr]
+
+
+        probList = [0.0 for i in range(0,16)]
 
         numPages = len(pageList)
-        if numPages == 1: 
-                # Found 1 page. Add the probability
-                probList[batchNum] += probIn
-                if pageList[0] == 5:
-                        return
+        if numPages == 1:
+                probList[batchNum] = 1.0
+                assert(pageList[0] < 5)
+
 
         for i in range(0,numPages):
+                nextList = list(pageList)
+                del nextList[i]
+
                 pageSize = pageList[i]
-                newList = pageList[:i] + pageList[i+1:]
+                for nextPage in range(pageSize+1,6):
+                        nextList.append(nextPage)
 
-                for newPage in range(pageSize+1,6):
-                        newList.append(newPage)
+                nextProbList = findProb(nextList, batchNum+1)
+                probList = map( lambda x,y: (x+y/numPages), probList, nextProbList)
 
-                findProb(newList, probList, probIn/numPages, batchNum+1)
-
-        return
+        ProbCache[pageListStr] = probList
+        return probList
 
                 
 
-        
 
-probList = [0 for i in range(0,16)]
+probList = findProb([1])
+answer = sum(probList[1:])    # Don't count first batch
+print round(answer,6)         # round to six decimal places
 
-findProb([1], probList, 1.0, 0)
-print probList
