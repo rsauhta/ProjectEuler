5fc3641adb8d18df905a8d5c78fcbed23fdd6930
Rohit Sauhta <rsauhta@yahoo.com>
2015-08-13 22:45:35 -0700

    Checkin to switch to another computer

---
+++ p072.py
@@ -0,0 +1,12 @@
+# https://projecteuler.net/problem=72
+# Counting fractions
+#
+
+
+# The problem is to find all relatively prime number sets in the give range. S
+#  To find relatively prime denominator for a given numerator, we can use approach similar to #70 which requires factoring the number 
+#   into prime factors
+#   However for this problem (and #70) we need an efficient way to factor a large sequence of numbers.
+
+
+
