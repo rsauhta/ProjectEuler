e99ad0c3bb9fc332cd157ca7b70c3cd28ecf2d86
Rohit Sauhta <rsauhta@yahoo.com>
2015-08-15 15:57:35 -0700

    Solution for #51. 6 digit primes are sufficient to find the match so there is no need to do heroics of trying to load 8 digit primes from a file.

--- p051.py
+++ p051.py
@@ -3,53 +3,72 @@
 #
 
 import util
-import pickle
-import timeit
-
-def storePrimes(filename, maxN):
-        primeList = util.GetPrimeList(maxN)
-        #print primeList
-        pFile = open(filename,'wb')
-        pickle.dump(primeList,pFile)
-        pFile.close()
-
-def readPrime(filename):
-        pFile = open(filename, 'rb')
-        primeList = pickle.load(pFile)
-        pFile.close()
-        return primeList
 
 
-def findAnswer(primeList):
+def findAnswer(primeList, primeDict):
         " find the lowest prime that creates a family of 8 primes"
 
-        count = 0
         for prime in primeList:
-                # check if prime has 3 repeating digits
-                numDigits=[0 for i in range(0,10)]
+
+                # count instance of each digit in the prime
+		# For every digit we also track the multiple to replace that number.
+		#  e..g for 8 in 1828 , we would have 101 as multiple. We can replace 8 by 3 by
+		#    p - 8*multiple + 3*multiple
+		#
+                digitCount=[0 for i in range(0,10)]
+		digitMultiple=[0 for i in range(0,10)]
                 tmp = prime
-                while prime > 0:
-                        mod = prime % 10
-                        numDigits[mod] += 1
-                        prime = int(prime/10)
+		multiple = 1
+                while tmp > 0:
+                        mod = tmp % 10
+                        tmp = int(tmp/10)
+
+                        digitCount[mod] += 1
+                        digitMultiple[mod] += multiple
+			multiple *= 10
+
+
+		# check if any digit has 3 instances. 
+		# some analysis shows that number of instances being replaced needs to be a multiple 
+		#   of 3 viz. 3, 6 or 9. If you replace 2 digits, sum of all digits will become multiple of 
+		#   3 more than 2 times= => those replacement instances would be multiple of 3 => not prime
+		# It is possible that the first instance found happens to have 4 instance, of which we would 
+		#  only replace 3 to get the answer. However I didn't handle that case but routine was able to find 
+		#  the right answer 
+		# 
                 for i in range(0,10):
-                        if numDigits[i]==3:
-                                print tmp
-                                count += 1
-                                if count > 10: 
-                                        break
+                        if digitCount[i]==3 or digitCount[i]==6:
+				baseNum = prime - digitMultiple[i]*i
+        			count = 0
+				for k in range(0,10):  # try all digits
+					tryNum = baseNum + digitMultiple[i]*k
+
+					# If we put zero as the leading digit, number would 
+					#  end up being smaller than prime. That's not allowed
+					# k==0 check ensures that we do stringfy only for the case of 
+					#  0 replacement
+					if k == 0 and  \
+					   len(str(tryNum)) < len(str(prime)):
+						continue
+
+					if tryNum in primeDict:
+						count += 1
+				if count >= 8:
+					#print "Found match", prime
+					for k in range(0,10):  # try all digits
+						tryNum = baseNum + digitMultiple[i]*k
+						if tryNum in primeDict:
+							return tryNum
 
 
+# start with 6 digit primes 
+primeList = util.GetPrimeList(10**6)
 
-#print findAnswer(8)
-#storePrimes(filename, 10**8)
-#print "stored" 
+primeDict = {}
+for p in primeList:
+	primeDict[p] = 1
 
-filename = "prime.pkl"
-primeList = readPrime(filename)
-print "done reading"
-print len(primeList)
-findAnswer(primeList)
 
-#print timeit.timeit('readPrime("prime.pkl")', number=1, setup='from __main__ import readPrime')
-        
+answer=findAnswer(primeList, primeDict)
+print "Answer =", answer
+
