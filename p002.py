92ec7e22089d2ca6509858f2faa67e747e031ef6
Rohit Sauhta <rsauhta@yahoo.com>
2017-03-23 21:07:41 -0700

    Add a solution using iterators and more functional approach

--- p002.py
+++ p002.py
@@ -12,6 +12,24 @@
 # four million, find the sum of the even-valued terms.
 #
 
+class FibIterator:
+        def __init__(self, maxAllowed):
+                self.a = 1
+                self.b = 1
+                self.max = maxAllowed
+        def __iter__(self):
+                return self
+        def __next__(self):
+                while (self.b <= self.max):
+                        temp = self.b
+                        self.b += self.a
+                        self.a = temp
+                        return temp
+                raise StopIteration()
+
+
+def generateFibSum2(maxAllowed):
+        return sum(filter(lambda x: x % 2 == 0, FibIterator(maxAllowed)))
 
 
 # Generates sum of even fibonacci numbers less than "maxAllowed"
@@ -22,7 +40,7 @@ def generateFibSum(maxAllowed):
         total = 0
 
         while (num2 <= maxAllowed) : 
-                print num2
+                #print num2
                 if (num2 % 2 == 0):
                         total += num2
                 temp = num1 + num2
@@ -33,6 +51,6 @@ def generateFibSum(maxAllowed):
 
 
 maxAllowed = 4000000
-#maxAllowed = 100
-print "for max range of %d, sum of even fibonacci = %d " % \
-        (maxAllowed, generateFibSum(maxAllowed))
+
+print("for max range of", maxAllowed, " sum of even fibonacci = ",generateFibSum(maxAllowed))
+print("for max range of", maxAllowed, " sum of even fibonacci = ",generateFibSum2(maxAllowed))
